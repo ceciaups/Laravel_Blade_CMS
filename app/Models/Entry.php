a2063a4be826dc9ea633875768e51fa917ac10fe
xRayMonDxp <raymond980711@gmail.com>
2023-04-13 21:49:54 -0400

    initial commit

---
+++ app/Models/Entry.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Entry extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'learned_at',
+    ];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class, 'type_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(USer::class, 'user_id');
+    }
+    
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+}
